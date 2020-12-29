c6f1c3a6891873e247a052cec5817bcb7a6816a7
Jean Lucas Lima <jeanleonino@gmail.com>
2020-12-28 23:43:53 -0300

    Hello World

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Oh wow, this is an app after all</Text>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png'
+        }}
+        style={{  width:200, height:200 }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f6fa',
     alignItems: 'center',
     justifyContent: 'center',
   },
